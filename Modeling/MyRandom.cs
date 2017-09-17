75a0e1bbdf207656f54cab34cd457d86e7cb8722
IgorZinovich450502 <anakin.sk@yandex.ru>
2017-09-17 16:16:11 +0300

    init commit

---
+++ Modeling/MyRandom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Modeling
+{
+    class MyRandom
+    {
+        private int Rn0, Rn, a, m;
+
+        public MyRandom(int R0, int a, int m)
+        {
+            this.a = a;
+            this.m = m;
+            this.Rn = R0;          
+        }
+
+        public double Next()
+        {
+            Rn = (Rn * a) % m;
+           
+            return Convert.ToDouble(Rn) / m;
+        }
+
+        public double Next(out int R)
+        {
+            Rn = (Rn * a) % m;
+            R = Rn;
+                      
+            return Convert.ToDouble(Rn) / m;
+        }
+
+        public bool check()
+        {
+            if (Rn == Rn0)
+                return false;
+            return true;
+        }
+
+
+    }
+}
